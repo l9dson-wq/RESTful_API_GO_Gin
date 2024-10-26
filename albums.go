95922ade00948d72c9c2fd1ca78bb9ed6a96cff2
l9dson-wq <andelsonnicolas@gmail.com>
2024-10-26 12:36:50 -0400

    little refactor of structures

---
+++ albums.go
@@ -0,0 +1,16 @@
+package main
+
+// album represents the data about the album.
+type album struct {
+    ID      string `json:"id"`
+    Title   string `json:"title"`
+    Artist  string `json:"artist"`
+    Price   float64 `json:"price"`
+}
+
+// Data of the albums.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
